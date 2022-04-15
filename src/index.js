560a60209cf9db2418816edd3aa8bef0eebc3114
ManuMxrales <manumxrales@gmail.com>
2022-04-15 12:41:35 -0500

    Se organizo de nuevo el proyecto

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './componentes/App/index.js';
 import {createRoot} from 'react-dom/client';
 
 const root = createRoot(document.getElementById('root'))
